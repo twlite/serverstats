24fd1f9beb84f3e04fa97c887f984f3cf9a3c5c4
NeverMinD <46562212+Aceag12@users.noreply.github.com>
2019-08-07 20:11:25 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js"); 
 const client = new Discord.Client();
 const db = require("quick.db");
-const botstats = new db.table('BotStats');
 
 client.on('ready', () => {
 
